b375151817dd20ec855bd182b0caefca3c4a4717
Tobias Burgherr <tburgherr@gmail.com>
2026-09-20 22:19:16 +0200

    Release ready

--- src/django_admin_runner/__init__.py
+++ src/django_admin_runner/__init__.py
@@ -2,7 +2,7 @@ from django_admin_runner.context import is_admin_runner, set_result_html
 from django_admin_runner.forms import FileField, FileOrPathField, FileOrPathWidget, ImageField
 from django_admin_runner.registry import register_command
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "register_command",
